183a5d5374cb904aa11e87bfee0b6f1f5a005012
Eduard-86 <77954505+Eduard-86@users.noreply.github.com>
2024-05-24 14:28:57 +0400

    init commit

---
+++ Private/EducationalLibraryPluginBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EducationalLibraryPluginBPLibrary.h"
+#include "EducationalLibraryPlugin.h"
+
+UEducationalLibraryPluginBPLibrary::UEducationalLibraryPluginBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UEducationalLibraryPluginBPLibrary::EducationalLibraryPluginSampleFunction(float Param)
+{
+	return -1;
+}
+
