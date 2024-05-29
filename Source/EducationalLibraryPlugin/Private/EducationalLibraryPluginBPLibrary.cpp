899e014d038798a10230e0b9bd4bbb088aa226a0
Eduard-86 <77954505+Eduard-86@users.noreply.github.com>
2024-05-29 17:24:50 +0400

    add first functions

--- Source/EducationalLibraryPlugin/Private/EducationalLibraryPluginBPLibrary.cpp
+++ Source/EducationalLibraryPlugin/Private/EducationalLibraryPluginBPLibrary.cpp
@@ -2,6 +2,7 @@
 
 #include "EducationalLibraryPluginBPLibrary.h"
 #include "EducationalLibraryPlugin.h"
+#include "Math/UnrealMathUtility.h"
 
 UEducationalLibraryPluginBPLibrary::UEducationalLibraryPluginBPLibrary(const FObjectInitializer& ObjectInitializer)
 : Super(ObjectInitializer)
@@ -9,8 +10,51 @@ UEducationalLibraryPluginBPLibrary::UEducationalLibraryPluginBPLibrary(const FOb
 
 }
 
-float UEducationalLibraryPluginBPLibrary::EducationalLibraryPluginSampleFunction(float Param)
+float UEducationalLibraryPluginBPLibrary::VectorLength(FVector vector)
 {
-	return -1;
+	// 
+	// FMath::Sqrt(10.f);
+
+	// 
+	// FMath::Pow(6.f, 2.f);
+
+	float SumPow = FMath::Pow(vector.X, 2.f) + FMath::Pow(vector.Y, 2.f) + FMath::Pow(vector.Z, 2.f);
+
+	return FMath::Sqrt(
+		FMath::Pow(vector.X, 2.f) +
+		FMath::Pow(vector.Y, 2.f) +
+		FMath::Pow(vector.Z, 2.f));
 }
 
+FVector UEducationalLibraryPluginBPLibrary::MultiplicationScalar(float scalar, FVector vector)
+{
+	return FVector(vector.X * scalar, vector.Y * scalar, vector.Z * scalar);
+}
+
+FVector UEducationalLibraryPluginBPLibrary::DivisionScalar(float scalar, FVector vector)
+{
+	return FVector(vector.X / scalar, vector.Y / scalar, vector.Z / scalar);
+}
+
+FVector UEducationalLibraryPluginBPLibrary::VectorNormalization(FVector vector)
+{
+	float temp = 1.f / VectorLength(vector);
+
+	return MultiplicationScalar(temp, vector);
+}
+
+FVector UEducationalLibraryPluginBPLibrary::VectorSum(FVector vector1, FVector vector2)
+{
+	return FVector(
+		vector1.X + vector2.X,
+		vector1.Y + vector2.Y,
+		vector1.Z + vector2.Z);
+}
+
+FVector UEducationalLibraryPluginBPLibrary::VectorDifference(FVector vector1, FVector vector2)
+{
+	return FVector(
+		vector1.X - vector2.X,
+		vector1.Y - vector2.Y,
+		vector1.Z - vector2.Z);
+}
--- Source/EducationalLibraryPlugin/Public/EducationalLibraryPluginBPLibrary.h
+++ Source/EducationalLibraryPlugin/Public/EducationalLibraryPluginBPLibrary.h
@@ -27,6 +27,88 @@ class UEducationalLibraryPluginBPLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_UCLASS_BODY()
 
-	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Execute Sample function", Keywords = "EducationalLibraryPlugin sample test testing"), Category = "EducationalLibraryPluginTesting")
-	static float EducationalLibraryPluginSampleFunction(float Param);
+	//UFUNCTION(
+	//	BlueprintCallable,
+	//	BlueprintPure,
+	//	Category = "Math | Byte",
+	//	meta = (
+	//		Keywords = "simona shr >> byte uint8",
+	//		DisplayName = "ShiftRight (byte)",
+	//		CompactNodeTitle = ">>"
+	//		)
+	//)
+	//static FORCEINLINE uint8 ShiftRight_Uint8(const uint8 Value, const int32 Shift) { return Value >> Shift; }
+
+
+	UFUNCTION(
+		BlueprintCallable,
+		BlueprintPure,
+		Category = "VecorMath",
+		meta = (
+			Keywords = "simona shr >> byte uint8",
+			DisplayName = "VShiftRight (byte)",
+			CompactNodeTitle = "V>>"
+			)
+	)
+	static float VectorLength(FVector vector);
+
+	UFUNCTION(
+		BlueprintCallable,
+		BlueprintPure,
+		Category = "VecorMath",
+		meta = (
+			Keywords = "simona shr >> byte uint8",
+			DisplayName = "VShiftRight (byte)",
+			CompactNodeTitle = "*"
+			)
+	)
+		static FVector MultiplicationScalar(float scalar, FVector vector);
+
+	UFUNCTION(
+		BlueprintCallable,
+		BlueprintPure,
+		Category = "VecorMath",
+		meta = (
+			Keywords = "simona shr >> byte uint8",
+			DisplayName = "VShiftRight (byte)",
+			CompactNodeTitle = "/"
+			)
+	)
+		static FVector DivisionScalar(float scalar, FVector vector);
+
+	UFUNCTION(
+		BlueprintCallable,
+		BlueprintPure,
+		Category = "VecorMath",
+		meta = (
+			Keywords = "simona shr >> byte uint8",
+			DisplayName = "VShiftRight (byte)",
+			CompactNodeTitle = "Norm"
+			)
+	)
+		static FVector VectorNormalization(FVector vector);
+
+	UFUNCTION(
+		BlueprintCallable,
+		BlueprintPure,
+		Category = "VecorMath",
+		meta = (
+			Keywords = "simona shr >> byte uint8",
+			DisplayName = "VShiftRight (byte)",
+			CompactNodeTitle = "VSum+"
+			)
+	)
+		static FVector VectorSum(FVector vector1, FVector vector2);
+
+	UFUNCTION(
+		BlueprintCallable,
+		BlueprintPure,
+		Category = "VecorMath",
+		meta = (
+			Keywords = "simona shr >> byte uint8",
+			DisplayName = "VShiftRight (byte)",
+			CompactNodeTitle = "VDifference-"
+			)
+	)
+		static FVector VectorDifference(FVector vector1, FVector vector2);
 };
